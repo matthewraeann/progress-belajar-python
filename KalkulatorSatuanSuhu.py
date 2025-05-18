652e194fd6369547f2cf73a3f6d02f323d0b67fc
Matthew Raeann Alexandra <matthewraeann888@gmail.com>
2025-05-19 00:03:21 +0700

    Update merapihkan
    Merapihkan menggunakan function

--- KalkulatorSatuanSuhu.py
+++ KalkulatorSatuanSuhu.py
@@ -1,75 +1,69 @@
-# 17 Mei 2025
+# 18 Mei 2025
 
-def is_number(value) :
-    try : 
-        float(value)
-        return True
-    except ValueError: 
-        return False
+import os
+os.system('clear')
+os.system('cls')
+
+def header(title) :
+    print(f'\n{title.center(56,'=')}\n')
+
+def convert_to_celcius(jenis,nilai) :
+    if jenis == 'R' :
+        celcius = nilai*5/4
+    elif jenis == 'F' :
+        celcius = (nilai-32)*5/9
+    elif jenis == 'K' :
+        celcius = nilai-273
+    else :
+        celcius = nilai
+    return celcius
+
+def input_suhu(jenis) :
+    while True :
+        try : 
+            nilai = float(input(f'\nMasukkan suhu dalam {jenis} : '))
+            celcius = convert_to_celcius(jenis,nilai)
+            return celcius
+        except ValueError :
+            print('\nHarap masukkan dalam bentuk angka')
+
+def formating(value) :
+        return f'{value:.2f}'.rstrip('0').rstrip('.')
+
+def display(satuan,nilai,lambang) :
+    print(f'Suhu dalam {satuan:<10} : {formating(nilai)}{lambang}')
+
+def check_continue() :
+    while True :
+        start_over = input('\nIngin mengkonversi data lain? (y/n) : ').lower().strip()
+        if start_over in ['y','n'] :
+            return start_over == 'y'
+        else:
+            print('\nData yang anda masukkan tidak valid!')
+
+header('KALKULATOR SUHU')
 
 while True :
     while True :
         print('Ketik "C" untuk Celcius\nKetik "R" untuk Reamur\nKetik "F" untuk Fahrenheit\nKetik "K" untuk Kelvin')
-        satuan = input('Pilih satuan suhu yang ingin di konversi (C/R/F/K) : ').upper()
-        if satuan == 'C' :
-            while True :
-                celcius = input('Masukkan suhu dalam celcius : ')
-                if is_number(celcius) == True : 
-                    celcius = float(celcius)
-                    break
-                else : print('\nHarap masukkan dalam bentuk angka')
-            break
-        elif satuan == 'R' :
-            while True :
-                reamur = input('Masukkan suhu dalam reamur : ')
-                if is_number(reamur) == True : 
-                    reamur = float(reamur)
-                    celcius = 5/4*reamur
-                    break
-                else : print('\nHarap masukkan dalam bentuk angka')
-            break
-        elif satuan == 'F' :
-            while True : 
-                fahrenheit = input('Masukkan suhu dalam fahrenheit : ')
-                if is_number(fahrenheit) == True : 
-                    fahrenheit = float(fahrenheit)
-                    celcius = (fahrenheit-32)*5/9
-                    break
-                else : print('\nHarap masukkan dalam bentuk angka')
-            break
-        elif satuan == 'K' :
-            while True : 
-                kelvin = input('Masukkan suhu dalam kelvin : ')
-                if is_number(kelvin) == True : 
-                    kelvin = float(kelvin)
-                    celcius = kelvin-273
-                    break
-                else : print('\nHarap masukkan dalam bentuk angka')
+        satuan = input('Pilih satuan suhu yang ingin di konversi (C/R/F/K) : ').upper().strip()
+        if satuan in ['C','R','F','K'] :
+            celcius = input_suhu(satuan)
             break
         else : 
-            print('\nData yang anda masukkan tidak valid')
+            print('\nData yang anda masukkan tidak valid!')
 
     reamur = 4/5*celcius 
     fahrenheit = 9/5*celcius + 32
     kelvin = celcius + 273
 
-    def formating(value) :
-        return f'{value:.2f}'.rstrip('0').rstrip('.')
+    print('\nHASIL KONVERSI')
+    display('celcius',celcius,'C')
+    display('reamur',reamur,'R')
+    display('fahrenheit',fahrenheit,'F')
+    display('kelvin',kelvin,'K')
 
-    celcius = formating(celcius)
-    reamur = formating(reamur)
-    fahrenheit = formating(fahrenheit)
-    kelvin = formating(kelvin)
+    if not check_continue() :
+        break
 
-    print(f'\nSuhu dalam celcius\t: {celcius}C')
-    print(f'Suhu dalam reamur\t: {reamur}R')
-    print(f'Suhu dalam fahrenheit\t: {fahrenheit}F')
-    print(f'Suhu dalam kelvin\t: {kelvin}K\n')
-
-    while True :
-        start_over = input('Ingin mengkonversi data lain? (y/n) : ').lower()
-        if start_over in ['y','n'] : break
-        else : print('Data yang anda masukkan tidak valid')
-    
-    if start_over == 'n' : break
-print(f'\n{'TERIMA KASIH'.center(50,'=')}\n')
+header('TERIMA KASIH')
